a682df7588a9df841013800a32529b6ccb5c7293
linming <flaaud159@gmail.com>
2018-11-21 06:34:45 -0800

    add some code

---
+++ quad_controller/src/quad_controller/pid_controller_lm.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2017 Udacity Inc.
+# All Rights Reserved.
+
+# Author: Brandon Kinman
+
+
+class PIDController:
+    def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 10):
+        #TODO
+	self.kp_ = kp
+	self.ki_ = ki
+	self.kd_ = kd
+	self.max_windup_ = max_windup
+
+    def reset(self):
+        #TODO
+	
+        pass
+
+    def setTarget(self, target):
+        #TODO
+        pass
+
+    def setKP(self, kp):
+        #TODO
+        pass
+
+    def setKI(self, ki):
+        #TODO
+        pass
+
+    def setKD(self, kd):
+        #TODO
+        pass
+
+    def setMaxWindup(self, max_windup):
+        #TODO
+        pass
+
+    def update(self, measured_value, timestamp):
+        #TODO
+        pass
+
+
